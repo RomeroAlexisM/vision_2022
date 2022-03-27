7016ab33cf57df46ee8e85d9a308dc6406209184
alexis <romeroalexism@gmail.com>
2022-03-27 17:19:18 -0300

    practico 2

---
+++ practico-2/practico-2.py
@@ -0,0 +1,23 @@
+import cv2 as cv
+
+def obtenerImagen():
+    return cv.imread('imagen.png')
+
+def aplicarUmbral(imagen):
+    imagen[imagen>210] = 255
+    imagen[imagen!=255] = 0
+    return imagen
+
+def guardarImagenModificada(imagen):
+    cv.imwrite("imagen-modificada.png", imagen)
+
+def iniciar():
+    print("Obteniendo Imagen")
+    imagen = obtenerImagen()
+    print('Imagen obtenida')
+    print('Aplicando umbral...')
+    imagenModificada = aplicarUmbral(imagen)
+    guardarImagenModificada(imagenModificada)
+    print('Imagen modificada guardada')
+
+iniciar()
